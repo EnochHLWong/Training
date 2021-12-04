d4bff4900afa9321e5d70e3c7e42c60c70cbd0b0
EnochHLWong <75590627+EnochHLWong@users.noreply.github.com>
2021-12-04 10:58:32 -0600

    Add files via upload

--- Dec4Training.java
+++ Dec4Training.java
@@ -2,9 +2,8 @@ import java.util.*;
 public class Dec4Training{
     public static void main(String[] args){
         System.out.println("Hello World");
-        //starSquare();
-        //printNum();
-        printPyramid();
+        starSquare();
+        printNum();
         
     }
     public static void starSquare(){
@@ -23,20 +22,5 @@ public class Dec4Training{
     public static void printNum(){
         int i = 1;
         System.out.print(i);
-    }
-    public static void printPyramid(){
-        int size = 7;
-        for(int i = 0; i < size / 2; i+=2){
-            for(int j = 0; j < i; j++){
-                System.out.print(" ");
-            }
-            for(int j = 0; j < (size / 2) * 2; j++){
-                System.out.print("*");
-            }
-            for(int j = 0; j < i; j++){
-                System.out.print(" ");
-            }
-            System.out.println();
-        }
     }
 }
